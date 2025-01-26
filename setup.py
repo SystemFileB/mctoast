242617e51e2ef571f9dec11e061b0b286abe10a9
SystemFileB <wangz7890@163.com>
2025-01-26 16:37:46 +0800

    setup更改

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="mctoast",
-    version="1.10",
+    version="1.10.1",
     description="把Minecraft的Toast带到现实里！",
     long_description=long_description,
     long_description_content_type="text/markdown",
